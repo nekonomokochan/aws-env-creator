1a727c59dd72f3278120e67d9356782ec7f83a4c
keitakn <keita.koga.work@gmail.com>
2019-03-20 01:40:41 +0900

    :white_check_mark: #40 Add fetchFromParameterStore test (do not specify profile)

--- test/integration/fetchFromParameterStore.spec.ts
+++ test/integration/fetchFromParameterStore.spec.ts
@@ -25,4 +25,28 @@ describe("fetchFromParameterStore", () => {
 
     expect(storeParamsList).toEqual(expectedList);
   });
+
+  it("should be able to fetch Params even without profile", async () => {
+    // In order to make this test successful,
+    // the same credentials as nekochans-dev must be set in the default profile of aws
+    const parameterStore = createParameterStoreClient({
+      region: AwsRegion.ap_northeast_1
+    });
+
+    const storeParamsList = await fetchFromParameterStore(
+      parameterStore,
+      "/dev/test-app/weather"
+    );
+
+    const expectedList = [
+      {
+        "sendgrid-api-key": "DummySendGridAPIKEY0001"
+      },
+      {
+        "slack-token": "DummySlackToken0001"
+      }
+    ];
+
+    expect(storeParamsList).toEqual(expectedList);
+  });
 });
