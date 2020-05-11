828b3487608018b5387dcfa05e5ab707d037dce6
keitakn <keita.koga.work@gmail.com>
2020-05-11 18:14:20 +0900

    :wrench: #55 fix error: Parsing error: "parserOptions.project" has been set for @typescript-eslint/parser.
    https://github.com/typescript-eslint/typescript-eslint/issues/967

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,29 +2,25 @@ module.exports = {
   env: {
     node: true,
     es6: true,
-    jest: true
+    jest: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:prettier/recommended"
-  ],
-  plugins: [
-    "@typescript-eslint"
-  ],
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  plugins: ["@typescript-eslint"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    "sourceType": "module",
-    "project": "./tsconfig.json",
-    "ecmaVersion": 2019
+    sourceType: "module",
+    project: "./tsconfig.json",
+    ecmaVersion: 2019,
+    createDefaultProgram: true,
   },
   rules: {
     "no-console": 0,
     "no-unused-vars": 0,
-    "no-undef": 0
+    "no-undef": 0,
   },
   settings: {
     node: {
-      tryExtensions: [".ts", ".tsx", ".js", ".jsx", ".json", ".node"]
-    }
-  }
+      tryExtensions: [".ts", ".tsx", ".js", ".jsx", ".json", ".node"],
+    },
+  },
 };
